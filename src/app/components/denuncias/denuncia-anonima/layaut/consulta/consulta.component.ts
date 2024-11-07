1ed00177d9e74f6f0de7943414f9ed8daa94fae9
Leona <leonardopoved@gmail.com>
2024-11-07 15:06:16 -0500

    se crearon las consultas y ya se peude evr el video y imaeg n y aaudio

--- src/app/components/denuncias/denuncia-anonima/layaut/consulta/consulta.component.ts
+++ src/app/components/denuncias/denuncia-anonima/layaut/consulta/consulta.component.ts
@@ -1,12 +1,41 @@
+// consulta.component.ts
 import { Component } from '@angular/core';
+import { ConsultaDenunciaResponse } from '../../interface/consultasDenunciasAnonimasInterface';
+import { DenunciasService } from '../../service/denuncias.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-consulta',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, HeaderComponent],
   templateUrl: './consulta.component.html',
-  styleUrl: './consulta.component.css'
+  styleUrls: ['./consulta.component.css']
 })
 export class ConsultaComponent {
+  claveUnica: string = '';
+  denuncia: any = null;
+  error: string = '';
 
+  constructor(private denunciasService: DenunciasService) { }
+
+  consultarDenuncia() {
+    this.denuncia = null;
+    this.error = '';
+
+    this.denunciasService.consultarDenunciaAnonima(this.claveUnica)
+      .subscribe(
+        (response: ConsultaDenunciaResponse) => {
+          this.denuncia = response.denuncia;
+          if (typeof this.denuncia.pruebas === 'string') {
+            this.denuncia.pruebas = [this.denuncia.pruebas];
+          }
+        },
+        (error) => {
+          this.denuncia = null;
+          this.error = error.error.error;
+        }
+      );
+  }
 }
