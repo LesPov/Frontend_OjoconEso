e705849846b9dcb5164b47c937273bbdbf4319ec
Leona <leonardopoved@gmail.com>
2025-01-29 12:30:06 -0500

    feat: actualizar dependencias de Angular y Leaflet, y cambiar la capa de mapa a CartoDB

--- src/app/components/denuncias/denuncia-anonima/layout/ubicacion/ubicacion.component.ts
+++ src/app/components/denuncias/denuncia-anonima/layout/ubicacion/ubicacion.component.ts
@@ -72,8 +72,8 @@ export class UbicacionComponent implements OnInit {
       zoomControl: false, // Desactiva el control de zoom
     }).setView([lat, lng], 13);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap contributors'
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+      attribution: '© OpenStreetMap contributors, © CartoDB'
     }).addTo(this.map);
 
     this.map.on('click', (e: L.LeafletMouseEvent) => {
